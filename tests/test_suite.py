21bbc69fc789de898d9c65f6a8a6931585d66d50
Merics Ferenc <mericsferenc@gmail.com>
2024-05-09 23:07:54 +0200

    add back others

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -22,45 +22,45 @@ class StackOverflowTestSuite(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-    # def test_login_logout(self):
-    #     login_page = LoginPage(self.driver)
-    #     home_page = HomePage(self.driver)
-    #     login_page.login(STACKOVERFLOW_EMAIL, STACKOVERFLOW_PASSWORD)
-    #     if not home_page.is_user_logged_in():
-    #         error_message = login_page.get_error_message()
-    #         print(f"Login failed with error: {error_message}")
-    #     self.assertTrue(home_page.is_user_logged_in(), "Login should be successful")
+    def test_login_logout(self):
+        login_page = LoginPage(self.driver)
+        home_page = HomePage(self.driver)
+        login_page.login(STACKOVERFLOW_EMAIL, STACKOVERFLOW_PASSWORD)
+        if not home_page.is_user_logged_in():
+            error_message = login_page.get_error_message()
+            print(f"Login failed with error: {error_message}")
+        self.assertTrue(home_page.is_user_logged_in(), "Login should be successful")
 
-    #     home_page.logout()
-    #     self.assertTrue(home_page.is_user_logged_out(), "Logout should be successful")
+        home_page.logout()
+        self.assertTrue(home_page.is_user_logged_out(), "Logout should be successful")
 
-    # def test_static_page_title(self):
-    #     home_page = HomePage(self.driver)
-    #     home_page.navigate()
-    #     self.assertEqual(self.driver.title, "Stack Overflow - Where Developers Learn, Share, & Build Careers")
+    def test_static_page_title(self):
+        home_page = HomePage(self.driver)
+        home_page.navigate()
+        self.assertEqual(self.driver.title, "Stack Overflow - Where Developers Learn, Share, & Build Careers")
 
-    # def test_multiple_pages(self):
-    #     home_page = HomePage(self.driver)
-    #     urls = [
-    #         "https://stackoverflow.com/questions",
-    #         "https://stackoverflow.com/jobs",
-    #         "https://stackoverflow.com/teams"
-    #     ]
-    #     for url in urls:
-    #         home_page.navigate_to(url)
-    #         self.assertTrue(self.driver.title)
+    def test_multiple_pages(self):
+        home_page = HomePage(self.driver)
+        urls = [
+            "https://stackoverflow.com/questions",
+            "https://stackoverflow.com/jobs",
+            "https://stackoverflow.com/teams"
+        ]
+        for url in urls:
+            home_page.navigate_to(url)
+            self.assertTrue(self.driver.title)
 
-    # def test_complex_xpath(self):
-    #     home_page = HomePage(self.driver)
-    #     home_page.navigate()
-    #     complex_element = home_page.find_element((By.XPATH, "//div//a[@href='/questions']"))
-    #     self.assertTrue(complex_element, "Complex XPath element should be found")
+    def test_complex_xpath(self):
+        home_page = HomePage(self.driver)
+        home_page.navigate()
+        complex_element = home_page.find_element((By.XPATH, "//div//a[@href='/questions']"))
+        self.assertTrue(complex_element, "Complex XPath element should be found")
 
-    # def test_explicit_wait(self):
-    #     home_page = HomePage(self.driver)
-    #     home_page.navigate()
-    #     element = home_page.find_element((By.XPATH, "//a[contains(@href, '/questions')]"), timeout=15)
-    #     self.assertTrue(element, "Element should be present with explicit wait")
+    def test_explicit_wait(self):
+        home_page = HomePage(self.driver)
+        home_page.navigate()
+        element = home_page.find_element((By.XPATH, "//a[contains(@href, '/questions')]"), timeout=15)
+        self.assertTrue(element, "Element should be present with explicit wait")
 
     def test_form_sending_with_user(self):
         login_page = LoginPage(self.driver)
