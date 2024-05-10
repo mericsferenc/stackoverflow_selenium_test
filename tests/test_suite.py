11a775b287825462a99ef6cca1117d48200277d6
Merics Ferenc <mericsferenc@gmail.com>
2024-05-10 20:05:07 +0200

    add multiple page test

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -40,16 +40,30 @@ class StackOverflowTestSuite(unittest.TestCase):
     #     home_page.navigate()
     #     self.assertEqual(self.driver.title, "Stack Overflow - Where Developers Learn, Share, & Build Careers")
 
-    # def test_multiple_pages(self):
-    #     home_page = HomePage(self.driver)
-    #     urls = [
-    #         "https://stackoverflow.com/questions",
-    #         "https://stackoverflow.com/jobs",
-    #         "https://stackoverflow.com/teams"
-    #     ]
-    #     for url in urls:
-    #         home_page.navigate_to(url)
-    #         self.assertTrue(self.driver.title)
+    def test_multiple_pages(self):
+        home_page = HomePage(self.driver)
+        pages = [
+            {
+                "url": "https://stackoverflow.co/teams/use-cases/engineering/",
+                "expected_text": "Build an efficient knowledge sharing culture"
+            },
+            {
+                "url": "https://stackoverflow.co/teams/use-cases/data-analytics/",
+                "expected_text": "We’ll organize the data, so you can do the science."
+            },
+            {
+                "url": "https://stackoverflow.co/teams/use-cases/devops/",
+                "expected_text": "Your system handles millions of interrupts, but you shouldn't have to."
+            },
+            {
+                "url": "https://stackoverflow.co/teams/use-cases/customer-support/",
+                "expected_text": "Leave no support question unanswered."
+            }
+        ]
+
+        for page in pages:
+            home_page.navigate_to(page["url"])
+            self.assertIn(page["expected_text"], self.driver.page_source)
 
     # def test_complex_xpath(self):
     #     home_page = HomePage(self.driver)
@@ -73,11 +87,11 @@ class StackOverflowTestSuite(unittest.TestCase):
     #     ask_question_page.set_title("Sample Question Title")
     #     ask_question_page.click_next()
     
-    def test_radio_button_interaction(self):
-        radio_page = RadioPage(self.driver)
-        radio_page.navigate()
-        radio_page.select_account_issue()
-        self.assertTrue(radio_page.is_account_issue_selected(), "Radio button Account Issue should be selected")
+    # def test_radio_button_interaction(self):
+    #     radio_page = RadioPage(self.driver)
+    #     radio_page.navigate()
+    #     radio_page.select_account_issue()
+    #     self.assertTrue(radio_page.is_account_issue_selected(), "Radio button Account Issue should be selected")
 
 if __name__ == '__main__':
     unittest.main()
